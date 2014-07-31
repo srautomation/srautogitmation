80faa9aefd5076350bcf4c2583338773d23a1d1e
Barak Bercovitz <barakber@gmail.com>
2014-07-31 16:47:21 +0300

    Tests: updated example test

--- tests/Example1.py
+++ tests/Example1.py
@@ -1,12 +1,13 @@
 from infrastructure.BaseTest import BaseTest
-import time
 import slash
 
 class Example1(BaseTest):
-    def test_waiting_window(self):
+    def test_limit_linux_cpu(self):
         result = None
         with self.tester.timeout(120):
-            self.device.linux.ui.run("/usr/bin/leafpad")
-            result = self.device.linux.ldtp.waittillguiexist("*frm*")
+            with self.tester.timeit.measure():
+                with self.linux.vitals.measure():
+                    self.linux.cmd("libreoffice")
+                    result = self.linux.ui.ldtp.waittillguiexist("LibreOffice")
+            slash.should.be(self.linux.vitals.measured.cpu.max < 0.6, True)
         slash.should.equal(result, 1)
-
