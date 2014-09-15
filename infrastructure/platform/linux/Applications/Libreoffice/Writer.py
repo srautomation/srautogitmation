cf5e289282db90941b80abd36bd0f90a7866ff32
Nadav Stiner <nadav.stiner@intel.com>
2014-09-15 13:44:08 +0300

    Libreoffice: Added Libreoffice subpackage, and implementation for Writer

---
+++ infrastructure/platform/linux/Applications/Libreoffice/Writer.py
@@ -0,0 +1,34 @@
+from Application import _Editor
+import time
+
+class Writer(_Editor):
+    def __init__(self, cmd, ui):
+        super(LibreOffice, self).__init__(cmd, ui, 'libreoffice --writer --norestore', 'killall oosplash')
+
+    def open(self, doc):
+        app = self._dogtail.tree.root.application('soffice')
+        app.child('Open').click()
+        time.sleep(4)
+        app.child(doc).point()
+        time.sleep(1)
+        app.child(doc).doubleClick()
+
+    def set_bold(self):
+        app = self._dogtail.tree.root.application('soffice')
+        app.child('Edit').click()
+        time.sleep(1)
+        app.child('Edit').child('Select All').point()
+        time.sleep(1)
+        app.child('Edit').child('Select All').click()
+        time.sleep(2)
+        app.child('Bold').click()
+
+    def set_italic(self):
+        app = self._dogtail.tree.root.application('soffice')
+        app.child('Edit').click()
+        time.sleep(1)
+        app.child('Edit').child('Select All').point()
+        time.sleep(1)
+        app.child('Edit').child('Select All').click()
+        time.sleep(2)
+        app.child('Italic').click()
