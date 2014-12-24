73bb8615f0970617a87cade14052d79568d28db9
Barak Bercovitz <barakber@gmail.com>
2014-12-24 10:02:39 +0200

    IMAPApp: fix imap usage to subest supported by imapapp

--- sr_automation/applications/IMAPApp.py
+++ sr_automation/applications/IMAPApp.py
@@ -1,57 +1,80 @@
-from imapclient import IMAPClient
 import email
 import time
-
 
 class IMAPApp(object):
     def __init__(self, linux):
         self._linux = linux
-        self._imap = None
-        self._info = None
+        self._imap_module = self._linux._rpyc.modules.imaplib
+        self._imap = self._imap_module.IMAP4("localhost")
+
         self._email = None
         self._password = None
         self._folder = None
-        self._ids  = None
-        self._msgs = None
+        self._count = None
 
+    def _assert_result(self, result):
+        assert result == "OK"
 
-    def choose_email(self, email, password=None):
+    def choose_email(self, email, password=""):
         self._email = email
         self._password = password
-        self._imap = IMAPClient("imap.gmail.com", ssl=True) #ssl=False) #TODO: fix this with real IMAPApp
-        self._imap.login(self._email, self._password)
-        return self
+        def authentication_callback(response):
+            return "\x00{} {}\x00{}".format(email, "", password)
+        result, _unused = self._imap.authenticate("PLAIN", authentication_callback)
+        self._assert_result(result)
 
     def choose_folder(self, folder):
         self._folder = folder
-        self._imap.select_folder(self._folder)
+        result, count = self._imap.select(folder)
+        self._assert_result(result)
+        self._count = int(count[0])
         return self
 
+    def folders(self):
+        result, _folders = self._imap.list()
+        self._assert_result(result)
+        folders = [x.split("/")[-1].replace('"', '').strip() for x in _folders]
+        return folders
+
+    def flags_string_to_bunch(self, flags_string):
+        return Bunch(
+            read    = ("\\Seen" in flags_string),
+            starred = ("\\Flagged" in flags_string),
+            deleted = ("\\Deleted" in flags_string),
+            answered = ("\\Answered" in flags_string),
+            attachment = False,
+            favorite   = False,
+            loaded     = False,
+            )
+
     def load(self):
-        self._ids  = self._imap.search("ALL")
-        self._msgs = self._imap.fetch(self._ids, ["FLAGS", "RFC822"])
+        result, data = self._imap.uid("fetch", "1:*", "(FLAGS)")
+        self._assert_result(result)
+        _tmp = [x.split(" ", 3) for x in data]
+        # dict indexed by uid
+        self._flags = {int(x[2]): self.flags_string_to_bunch(x[3]) for x in q}
+
+        # for now, fetch all TODO: think about it
+        uids_text = ','.join(map(str, self._flags.keys())) 
+        result, data = self._imap.uid("fetch", uids_text, "(RFC822)")
+        self._assert_result(result)
+        # dict indexed by uid
+        self._msgs = {int(x[0].split(" ", 3)[2]): email.message_from_string(x[1]) for x in b[::2]}
+
         return self
 
     def messages(self):
-        emails = [(m["FLAGS"], email.message_from_string(m["RFC822"])) for m in self._msgs.values()[1:]]
         _messages = [Bunch(
-            time  = dateutil.parser.parse(m[1]["date"]),
-            from_ = m[1]["from"],
-            to    = m[1]["to"],
+            time  = dateutil.parser.parse(mail["date"]),
+            from_ = mail["from"],
+            to    = mail["to"],
             cc    = None,
-            subject = m[1]["subject"],
-            flags = Bunch(
-                read    = ("\\Seen" in m[0]),
-                starred = ("\\Flagged" in m[0]),
-                deleted = ("\\Deleted" in m[0]),
-                attachment = False,
-                favorite   = False,
-                loaded     = False,
-                ),
+            subject = mail["subject"],
+            flags = self._flags[uid],
             body = Bunch(
-                text = m[1].get_payload()[0].get_payload(),
-                html = m[1].get_payload()[1].get_payload(),
+                text = mail.get_payload()[0].get_payload(),
+                html = mail.get_payload()[1].get_payload(),
                 ),
             )
-            for m in emails]
+            for (uid, mail) in self._msgs]
         return _messages
