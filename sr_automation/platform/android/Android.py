80356455e15eb0debecb8fae5d621f70c1d69362
Barak Bercovitz <barakber@gmail.com>
2015-02-11 10:38:39 +0200

    sl4a: initial commit

--- sr_automation/platform/android/Android.py
+++ sr_automation/platform/android/Android.py
@@ -3,11 +3,14 @@ from Activities import Activities
 from Processes import Processes
 from NetInterfaces import NetInterfaces
 import uiautomator
+import sl4a
+import time
 
 from logbook import Logger
 log = Logger("Android")
 
 class Android(object):
+    SL4A_PORT = 23456
     def __init__(self, device_id):
         self._device_id = device_id
         self._ui = uiautomator.Device(serial = self._device_id)
@@ -18,12 +21,26 @@ class Android(object):
         self._processes = Processes(self._adb)
         self._activities = Activities(self._adb)
         self._interfaces = NetInterfaces(self._adb)
+        self._sl4a = None
+
+    def start_sl4a(self):
+        self.adb.cmd("shell", "am", "start", "-a", "com.googlecode.android_scripting.action.LAUNCH_SERVER", 
+                      "-n", "com.googlecode.android_scripting/.activity.ScriptingLayerServiceLauncher", 
+                      "--ei", "com.googlecode.android_scripting.extra.USE_SERVICE_PORT", str(Android.SL4A_PORT))
+        time.sleep(2)
+        self.adb.forward(Android.SL4A_PORT, Android.SL4A_PORT)
+        self._sl4a = sl4a.Android(("localhost", Android.SL4A_PORT))
+
+    def stop_sl4a(self):
+        pass
 
     def start(self):
         pass
+        #self.start_sl4a()
 
     def stop(self):
         pass
+        #self.stop_sl4a()
 
     @property
     def adb(self):
@@ -32,6 +49,10 @@ class Android(object):
     @property
     def ui(self):
         return self._ui
+
+    @property
+    def sl4a(self):
+        return self._sl4a
 
     @property
     def battery(self):
