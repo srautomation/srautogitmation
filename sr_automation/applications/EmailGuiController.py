e5ce526ec1d1940443daabe25afe902583989780
Omri <beomri@gmail.com>
2015-03-26 10:38:14 +0200

    added more functions to EmailGuiController

--- sr_automation/applications/EmailGuiController.py
+++ sr_automation/applications/EmailGuiController.py
@@ -1,4 +1,5 @@
 import uiautomator
+import time
 
 class EmailGuiController(object):
 
@@ -9,19 +10,24 @@ class EmailGuiController(object):
         self.y_top = 200
 
     def send(self, to, subject, body, attachments = []):
+        self.choose_folder('outbox')
         self.write_mail(to, subject, body, attachments)
         self.d(resourceId = 'com.android.email:id/send').click()
+        if not body:
+            self.d(text="Send").click()
+        self.wait_outbox_empty()
 
     def write_mail(self, to, subject, body, attachments = []):
         self.main_view()
         self.d(resourceId = 'com.android.email:id/compose').click()
         self.d.wait.idle()
-        if type(to) == type('s'):
+        if isinstance(to, basestring):
             self.d(resourceId = 'com.android.email:id/to_content').set_text(to)
         else:
             self.d(resourceId = 'com.android.email:id/to_content').set_text(','.join(to))
         self.d(resourceId = 'com.android.email:id/subject').set_text(subject)
-        self.d(resourceId = 'com.android.email:id/body').set_text(body)
+        body_textbox = self.d(resourceId="com.android.email:id/body")
+        body_textbox.set_text(body)
 
     def save_draft(self, to = '', subject = '', body = '', attachments = []):
         self.write_mail(to, subject, body, attachments)
@@ -35,12 +41,30 @@ class EmailGuiController(object):
         self.d(descriptionContains = 'edit').click()
         self.d(resourceId = 'com.android.email:id/send').click()
 
+    def reply(self, body):
+        self.d(resourceId = "com.android.email:id/reply").click()
+        self.d(text = "Compose email").set_text(body)
+        self.d(resourceId = 'com.android.email:id/send').click()
+
+    def reply_all(self, body):
+        self.d(description = "More options", resourceId = "com.android.email:id/overflow").click()
+        self.d(text = "Reply all").click()
+        self.d(text = "Compose email").set_text(body)
+        self.d(resourceId = 'com.android.email:id/send').click()
+
+    def forward(self, to, body):
+        self.d(description = "More options", resourceId = "com.android.email:id/overflow").click()
+        self.d(text = "Forward").click()
+        self.d(text = "To").set_text(to)
+        self.d(text = "Compose email").set_text(body)
+        self.d(resourceId = 'com.android.email:id/send').click()
+
     def main_view(self):
         self.open_email_app()
         if self.d(text = 'Settings', resourceId = 'android:id/title').exists:
             self.d.press.menu()
-        while self.d(descriptionContains = 'up').exists:
-            self.d(descriptionContains = 'up').click()
+        while self.d(descriptionContains = 'Navigate up').exists:
+            self.d(descriptionContains = 'Navigate up').click()
             self.d.wait.update()
         if self.d(resourceId = 'android:id/action_mode_close_button').exists:
             self.d(resourceId = 'android:id/action_mode_close_button').click()
@@ -85,7 +109,7 @@ class EmailGuiController(object):
             return False
         return True
 
-    def move_to_folder(self, contains, folder):
+    def move_message_to_folder(self, contains, folder):
         self.choose_message(contains)
         self.d(description = 'Move to').click()
         self.d(resourceId = 'android:id/select_dialog_listview').swipe.down()
@@ -142,15 +166,29 @@ class EmailGuiController(object):
         self.d.press.enter()
 
     def delete_message(self, contains):
-        self.main_view()
         if self.choose_message(contains):
             self.d(descriptionContains = 'delete').click()
+
+    def delete_first_message(self):
+        self.choose_first_message()
+        self.d(descriptionContains = 'delete').click()
+
+    def delete_from_trash(self, contains):
+        self.choose_folder('trash')
+        self.delete_message(contains)
+
+    def delete_first_from_trash(self):
+        self.choose_folder('trash')
+        self.delete_first_message()
 
     def enter_message(self, contains):
         self.find_message(contains).click()
 
     def choose_message(self, contains):
         self.find_message(contains).long_click()
+
+    def choose_first_message(self):
+        self.d(resourceId="android:id/list").child(index="0").long_click()
 
     def find_message(self, contains):
         if self.d(descriptionContains = contains).exists:
@@ -178,6 +216,43 @@ class EmailGuiController(object):
             self.d(description = 'Mark unread').click()
         self.d.press.back()
 
+    def add_star(self):
+        self.choose_first_message()
+        self.d(descriptionContains="options").click()
+        self.d(text="Add star").click()
+        self.d.press.back()
+
+    def remove_star(self):
+        self.choose_first_message()
+        self.d(descriptionContains="options").click()
+        self.d(text="Remove star").click()
+        self.d.press.back()
+
+    def add_account(self, email, password):
+        self.main_view()
+        self.d(description = "More options", className = "android.widget.ImageButton").click()
+        self.d(text="Settings").click()
+        self.d(text="Add account").click()
+        self.d(text="Email address").set_text(email)
+        self.d(resourceId="com.android.email:id/account_password").set_text(password)
+        self.d(text="Next").click()
+        while self.d(textContains="Validating server settings").exists: pass
+        self.d(text="Next").click()
+        while self.d(textContains="creating account").exists: pass
+        self.d(text="Next").click()
+
+    def remove_account(self, email):
+        self.d.press.home()
+        self.d.open.quick_settings()
+        self.d(textContains='settings').click()
+        self.wait()
+        self.scroll_down()
+        self.d(text="IMAP").click()
+        self.d(text=email).click()
+        self.d(descriptionContains="options").click()
+        self.d(text="Remove account").click()
+        self.d(text="Remove account").click()
+
     def scroll_down(self):
         self.d.swipe(self.x_middle, self.y_bottom, self.x_middle, self.y_top, steps = 30)
         self.d.wait.idle()
@@ -185,25 +260,40 @@ class EmailGuiController(object):
     def scroll_up(self):
         self.d.swipe(self.x_middle, self.y_top, self.x_middle, self.y_bottom, steps = 30)
         self.d.wait.idle()
-    """
-    def count_messages(self, folder = None):
-        messages = []
-        if not self.click_first_message(folder):
-            return len(messages)
-        self.d(resourceId = 'com.android.email:id/subject').wait.exists(timeout = 8000)
-        current_m = self.d.dump()
-        while len(messages) == 0 or current_m != messages[-1]:
-            messages.append(current_m)
-            self.scroll_to_next_message()
-            self.d(resourceId = 'com.android.email:id/subject').wait.exists(timeout = 8000)
-            current_m = self.d.dump()
-        return len(messages)
-    """
+
+    def account_settings(self, email):
+        self.main_view()
+        self.d(descriptionContains="options").click()
+        self.d(text="Settings").click()
+        self.d(textContains=email).click()
+
+    def change_name(self, email, name):
+        self.account_settings(email)
+        self.d(text="Your name").click()
+        self.d(resourceId="android:id/edit").set_text(name)
+        self.d(text="OK").click()
+        self.main_view()
+
+    def change_sig(self, email,  sig):
+        self.account_settings(email)
+        self.d(text="Signature").click()
+        self.d(resourceId="android:id/edit").set_text(sig)
+        self.d(text="OK").click()
+        self.main_view()
+
+    def wait_outbox_empty(self):
+        self.choose_folder('outbox')
+        #self.d(text="No messages.").wait.exists()
+        while not self.d(text="No messages.").exists: pass
+
 
 def main():
-    import IPython
+    import IPython, loremipsum, time
     am = EmailGuiController(uiautomator.device)
-    IPython.embed()
+    to='srusertest@gmail.com'
+    for i in range(12):
+        am.send(to, loremipsum.get_sentence().encode('utf8')," ".join(loremipsum.get_sentences(4)).encode('utf8'))
+    #IPython.embed()
 
 if __name__ == '__main__':
     main()
