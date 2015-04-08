d87b280d714d687d784e0569a61c9b7cac96f4d6
Barak Bercovitz <barakber@gmail.com>
2015-04-08 10:56:07 +0300

    Linux: add ip property

--- sr_automation/platform/linux/Linux.py
+++ sr_automation/platform/linux/Linux.py
@@ -11,6 +11,11 @@ class Linux(object):
         self._modules = modules
         self._rpyc = rpyc
         self._shell = Shell(self._modules, self._rpyc)
+        if self._rpyc is not None:
+            self._ip = self.cmd("netstat -na | grep ':18812.*ESTABLISHED' | head -1 | tr ':' ' ' | awk {'print $4'}", shell=True).stdout.read().strip()
+        else:
+            self._ip = "127.0.0.1"
+
 
     def __del__(self):
         if self._rpyc is not None:
@@ -35,6 +40,10 @@ class Linux(object):
     def ui(self):
         return self._ui
 
+    @property
+    def ip(self):
+        return self._ip
+
     def cmd(self, cmdline, *args, **kw):
         return self.shell.cmd(cmdline, *args, **kw)
 
@@ -45,4 +54,7 @@ if __name__ == "__main__":
     modules = Bunch(os=os, psutil=psutil)
     
     linux = Linux(modules=modules)
+    print linux.ip
     print linux.cmd("ls -la").stdout.read()
+    import IPython
+    IPython.embed()
