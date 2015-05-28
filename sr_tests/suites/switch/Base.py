b965a05a5bbbb86edfa6dcdda94dd5a358e6aea2
Omri <beomri@gmail.com>
2015-05-28 10:51:28 +0300

    fixed switch/Base.py test

--- sr_tests/suites/switch/Base.py
+++ sr_tests/suites/switch/Base.py
@@ -8,16 +8,13 @@ import subprocess
 
 class SwitchBaseTest(BaseTest):
 
-    times_to_switch = 5
+    times_to_switch = 10
 
     def test_many_switches(self):
         for i in range(self.times_to_switch):
             slash.logger.info('Cycle #%s' % (i+1))
             slash.g.sunriver.switch_to_android.switch()
             slash.g.sunriver.desktop.switch_to_desktop()
-            time.sleep(8)
 
     def test_switch(self):
         slash.g.sunriver.switch_to_android.switch()
-        time.sleep(5)
-
