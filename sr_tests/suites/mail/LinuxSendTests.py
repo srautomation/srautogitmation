413bd27476bce68087ea58a1b6a48de574702c08
Omri <beomri@gmail.com>
2015-05-31 09:52:55 +0300

    fixed LinuxSendTests.py to wait before comparing

--- sr_tests/suites/mail/LinuxSendTests.py
+++ sr_tests/suites/mail/LinuxSendTests.py
@@ -2,13 +2,21 @@ from BaseSendTests import BaseSendTests
 from sr_automation.platform.linux.applications.Mail.GUI import LinuxMailGUI
 import slash
 import slash.log
-import IPython
+import time
 
 class LinuxEmailSendTests(BaseSendTests):
 
     @slash.hooks.session_start.register
     def start_linux_gui():
         slash.g.mail.linuxGUI = LinuxMailGUI(slash.g.sunriver.linux)
+
+    def before(self):
+        super(LinuxEmailSendTests, self).before()
+
+    def after(self):
+        super(LinuxEmailSendTests, self).before()
+        time.sleep(2)
+
 
     def send_mail(self, to, subject, body, attachments = []):
         #slash.g.mail.linuxGUI.send(to,subject,body,attachments)
