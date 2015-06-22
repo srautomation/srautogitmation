e21ae691e2f1e27c5f056e21ead48fa25d2b6f17
Omri <beomri@gmail.com>
2015-06-22 14:45:21 +0300

    Linux:Mail:GUI added a basic function to start icedove

--- sr_automation/platform/linux/applications/Mail/GUI.py
+++ sr_automation/platform/linux/applications/Mail/GUI.py
@@ -1,6 +1,7 @@
 class LinuxMailGUI(object):
     def __init__(self, linux):
         self._linux = linux
+        self._dogtail = self._linux.ui.dogtail
 
     def send(self, to, subject, body, attachments=[]):
         to_list = ",".join(to)
@@ -10,6 +11,15 @@ class LinuxMailGUI(object):
                                                                                   )
         self._linux.shell.shell(command)
 
+
+    def start_icedove(self):
+        self._dogtail.procedural.run('icedove')
+        self._icedove = self._dogtail.tree.root.application('Icedove')
+        pwrd = self._icedove.child(roleName='dialog').child(roleName='password text') #delete those lines after the icedove password bug has been resolved
+        pwrd.text = '111'                                                             #
+        ok_button = self._icedove.child(roleName='dialog').child(name='OK')           #
+        ok_button.click()                                                             #
+
     
 if __name__ == "__main__":
     from sr_automation.platform.sunriver.Sunriver import Sunriver
@@ -17,6 +27,7 @@ if __name__ == "__main__":
     sunriver.desktop.start()
     sunriver.linux.start()
     mail_gui = LinuxMailGUI(sunriver.linux)
+    mail_gui.start_icedove()
     import IPython
     IPython.embed()
 
