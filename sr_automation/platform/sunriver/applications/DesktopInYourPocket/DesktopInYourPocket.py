34cbd3f0168e0d666032d99e4f0b316184ad1272
Dor Libman <Dorlibman@gmail.com>
2015-06-28 11:43:47 +0300

    sunriver:desktopinyourpocket: fix module to use ui instead of scripts

--- sr_automation/platform/sunriver/applications/DesktopInYourPocket/DesktopInYourPocket.py
+++ sr_automation/platform/sunriver/applications/DesktopInYourPocket/DesktopInYourPocket.py
@@ -20,19 +20,32 @@ class DesktopInYourPocket(object):
         self._android.ui.press.menu()
         self._android.ui(textContains=pattern).click.wait()
 
+    def openApp(self):
+        if not self._android.ui(packageName="com.intel.desktopinyourpocket").exists:
+            self._android.ui.press.home()
+            self._android.ui(description='Apps').click()
+            self._android.ui(text="Intel's Desktop In Your Pocket").click()
+        
+
     def start(self):
         #self.click_menu(pattern=DesktopInYourPocket.BUTTON_START)
-        self._android.cmd('shell /data/srctl start')
+        #self._android.cmd('shell /data/srctl start')
+        self.openApp()
+        self._android.ui(text="Start Desktop").click()
 
     def stop(self):
         #self.click_menu(pattern=DesktopInYourPocket.BUTTON_STOP)
-        self._android.cmd('shell /data/srctl stop')
-
+        #self._android.cmd('shell /data/srctl stop')
+        self.openApp()
+        self._android.ui(text="Shutdown Desktop").click()
+       
     def switch_to_desktop(self):
         if not self.is_desktop_running():
             #self.click_menu(pattern=DesktopInYourPocket.BUTTON_SWITCH_TO_DESKTOP)
             log.info('Switching to desktop')
-            self._android.cmd('shell /data/srctl switch desktop')
+            #self._android.cmd('shell /data/srctl switch desktop')
+            self.openApp()
+            self._android.ui(text="Switch to Desktop").click()
             sleep(8)
 
 
@@ -43,13 +56,8 @@ class DesktopInYourPocket(object):
         return bool(int(output))
 
 if __name__ == "__main__":
-    import sys; sys.path.append("../../../android")
-    import time
-    from Android import Android
+    from sr_automation.platform.android.Android import Android
     device_id = Android.devices().keys()[0]
     android   = Android(device_id)
     desktop   = DesktopInYourPocket(android)
-    desktop.start()
-    time.sleep(8)
     desktop.stop()
-
