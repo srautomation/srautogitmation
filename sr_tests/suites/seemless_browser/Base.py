ad09d6d092083583087583af20bea5daa3be2f8c
dlibmanx <sunriver1993@gmail.com>
2016-03-21 10:07:02 +0200

    test changed

--- sr_tests/suites/seemless_browser/Base.py
+++ sr_tests/suites/seemless_browser/Base.py
@@ -19,37 +19,6 @@ class SeemlessBrowserTest(BaseTest):
         log.warn("Starting LibreOffice")
         slash.g.writer = Writer(slash.g.sunriver.linux)
         slash.g.writer.start(option='/tmp/dogtail-BigScreen/chromeShot.png')
-    
-    def test_chromium(self):
-        self.start_chrome()
-        log.info("Testing Chromium")
-        slash.g.chromium.open_youtube_video("watch?v=YQHsXMglC9A")
-        time.sleep(15) # allow video loading
-        slash.g.chromium.skip_add()
-        slash.g.sunriver.linux.ui.dogtail.utils.screenshot(file='chromeShot.png', timeStamp=False)
-        time.sleep(5)
-        slash.g.chromium.pause_video()
-        time.sleep(5)
-        slash.g.chromium.play_video()
-        time.sleep(3)
-        slash.g.chromium.youtube_fullscreen()
-        time.sleep(5)
-        slash.g.chromium.escape()
-        slash.g.chromium._driver.execute_script("window.open('http://www.cnn.com');")
-        time.sleep(12) # let the website upload
-        slash.g.chromium._driver.execute_script("window.open('http://www.nba.com');")
-        time.sleep(12) # let the website upload
-        for i in range(4):
-            time.sleep(2)
-            slash.g.chromium.switch_tab()
-        slash.g.chromium.stop()
-
-    def test_libre(self):
-        self.start_libre_with_screenshot()
-        slash.g.writer.write_text('inserting text')
-        slash.g.writer.choose_slide()
-        time.sleep(3)#wait for save button to appear
-        slash.g.writer.save_as(file_name='file')       
 
     def compare_cycle(self,cycle_number, cycle):
         slash.should.be(cycle_number, cycle)
