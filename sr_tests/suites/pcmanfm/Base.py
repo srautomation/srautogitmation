902c8b15dadee86aa2dc82b4b4bc21c50d8f46df
dlibmanx <sunriver1993@gmail.com>
2016-03-21 10:15:43 +0200

    changes due to GTK problems

--- sr_tests/suites/pcmanfm/Base.py
+++ sr_tests/suites/pcmanfm/Base.py
@@ -10,12 +10,12 @@ class PcManFMBaseTest(BaseTest):
     def before(self):
         super(PcManFMBaseTest, self).before()
 
-    def start_pcmanfm(self):
+    def start_pc(self):
         slash.g.pcmanfm = Pcmanfm(slash.g.sunriver.linux)
-        slash.g.pcmanfm.start()
+        slash.g.pcmanfm.start_pcmanfm()
 
     def test_pcmanfm(self):
-        self.start_pcmanfm()
+        self.start_pc()
         slash.g.pcmanfm.new_folder()
         slash.g.pcmanfm.new_file()
         slash.g.pcmanfm.goto('Documents')
