8d89255fc5071d2fa5cbe779499cdfed8256d8c3
yaniv shachor <sunriver1993@gmail.com>
2016-03-29 19:22:47 +0300

    move find subimage in image to ImageTools

--- sr_tests/suites/settings/sound.py
+++ sr_tests/suites/settings/sound.py
@@ -1,33 +1,20 @@
-import cv2
 import time
 from sr_tests.base.Base import BaseTest
 import slash
 from sr_automation.platform.linux.applications.Settings.Settings import Settings
 from sr_automation.utils.ImageTools import ImageTools
-from sr_automation.utils.TakeSnapshot import TakeSnapshot
+
 from logbook import Logger
 log = Logger("SOUND")
 
 
 class SoundBaseTest(BaseTest):
-    SNAPSHOT_PATH = "/tmp/"
     SNAPSHOT_FULL_VOLUME="fullScreen-sound-full-volume.png"
     SNAPSHOT_MUTE = "fullScreen-sound-mute-volume.png"
-    ICON_MUTE_PATH = "/home/automation2/sr_automation/automation-screenshots/sound-mute.png"
-    ICON_FULL_VOLUME_PATH = "/home/automation2/sr_automation/automation-screenshots/sound-full-volume.png"
-    CHROOT_PATH = "/data/sunriver/fs/limited/"
-    DIST_PATH = "/tmp/"
+    ICON_MUTE_PATH = "/tmp/automation-screenshots/sound-mute.png"
+    ICON_FULL_VOLUME_PATH = "/tmp/automation-screenshots/sound-full-volume.png"
     initialized = False
-
     
-    crop_coordinates_dict = {
-                             "y_min" : 0, 
-                             "y_max" : 50,
-                             "x_min" : 0,
-                             "x_max" : 1280, #TODO: use screen resolution width, not constant  
-                             "z_min": 0,
-                             "z_max" : 3
-                            }
     
     def before(self):
         if not self.initialized:
@@ -49,15 +36,22 @@ class SoundBaseTest(BaseTest):
         time.sleep(2)
         self.sound.change_output_volume_level(100)
         self.check_volume_icon(self.SNAPSHOT_FULL_VOLUME,self.ICON_FULL_VOLUME_PATH)
-
-    def check_volume_icon(self,image_name,path_subimage):
-        snapshot = TakeSnapshot(self.settings.linux.shell)
-        snapshot.take_snapshot(self.SNAPSHOT_PATH, image_name)
-        ImageTools.copy_file_from_device(self.CHROOT_PATH+self.SNAPSHOT_PATH+image_name, self.DIST_PATH)
-        time.sleep(2)
-        image = cv2.imread(self.DIST_PATH + image_name)
-        cropped_image = ImageTools.crop_image(image, self.crop_coordinates_dict)
-        sub_image = cv2.imread(path_subimage)
-        stats = ImageTools.find_sub_image_in_image(cropped_image, sub_image)
-        log.info("best match percentage %f location (%f,%f)"% (stats.max_value,stats.max_location[0],stats.max_location[1]))
+    
+    def check_volume_icon(self,image_name,subImage_path):
+        boundries = self.find_crop_boundries()
+        stats = ImageTools.find_sub_image_in_image(image_name, subImage_path, boundries)
         assert stats.max_value > 0.9 , "icon %s didn't work" % (image_name)  
+        
+    def find_crop_boundries(self):
+        cmd="xdpyinfo | grep dimensions | awk '{print $2}' | cut -d 'x' -f1"
+        image_width_string=slash.g.sunriver.linux.shell.runCommandWithReturnValue(cmd)
+        image_width = int(image_width_string)
+        crop_coordinates_dict = {
+                             "y_min" : 0, 
+                             "y_max" : 50,
+                             "x_min" : 0,
+                             "x_max" : image_width,   
+                             "z_min": 0,
+                             "z_max" : 3
+                            }
+        return crop_coordinates_dict
