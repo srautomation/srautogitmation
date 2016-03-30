53411ff034940dcdc577a8d885e33bf518bae272
labuser <sunriver1993@gmail.com>
2016-03-30 18:05:34 +0300

    new test type with snapshots

--- sr_tests/suites/app_launcher/AppLauncher.py
+++ sr_tests/suites/app_launcher/AppLauncher.py
@@ -8,31 +8,36 @@ class AppLaunchTest(LauncherBaseTest):
 
     @slash.hooks.session_start.register
     def start_AppLauncher():
-        slash.g.stress_run = 3
+        slash.g.stress_run = 1
         log.info('Opening AppLauncher')
         slash.g.cycle = 0
 
     def folders_open(self):
 	folder_list = ['Files', 'Pictures', 'Videos', 'Music']
 	for folder in folder_list:
-		self.open_launcher()
-		self.open_folders(folder)
+		self.open_from_applauncher_by_icon_name(folder)
 		time.sleep(4)
 
+    def check_app_opened_from_launcher(self, i_appName):
+        self.open_from_applauncher_by_icon_name(i_appName)
+        appPid = self.convert_app_name_to_pid(i_appName)
+        appOpened = self.check_app_by_pid(appPid)
+        slash.should.be(appOpened, True)
+        return appPid
 
     def test_launcher(self):
         for i in range(slash.g.stress_run):
             slash.logger.info("cycle #%s" % (i + 1))
 	    self.folders_open()
-            self.close_app()
+            #assert like contextual
+            self.close_all_apps()
+            #assert like contextual
             time.sleep(3)
-            self.open_app_sidepanel(stimes=1)
-	    self.open_contextual()
+            app_name = 'Galculator'
+            app_pid = self.check_app_opened_from_launcher(app_name)
+	    self.contextual_on_calc()
 	    time.sleep(7)
-	    self.open_search('Chro')
-	    self.open_app_sidepanel()
+	    searchedChromium = self.search_chromium()
+            slash.should.be(searchedChromium, True)
    	    slash.g.cycle +=1
 	self.compare_cycle(slash.g.cycle, slash.g.stress_run)
-	
-	 
-
