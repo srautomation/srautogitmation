0c4ed4fcb7d3af71757613769986bdd5de278734
labuser <sunriver1993@gmail.com>
2016-04-03 14:43:07 +0300

    finalized tests

--- sr_tests/suites/app_launcher/AppLauncher.py
+++ sr_tests/suites/app_launcher/AppLauncher.py
@@ -3,15 +3,13 @@ from Base import LauncherBaseTest
 import time
 from logbook import Logger
 from sr_automation.utils.ProcessManager import ProcessManager
-log = Logger("StressBaseInsideTest")
+log = Logger("App Launcher Suite")
 
 class AppLaunchTest(LauncherBaseTest):
 
     @slash.hooks.session_start.register
     def start_AppLauncher():
-        slash.g.stress_run = 1
-        log.info('Opening AppLauncher')
-        slash.g.cycle = 0
+        log.info('Starting "Search, App Launcher and Side Panel" Suite')
 
     def open_app_from_launcher(self, i_appName):
         self.open_from_applauncher_by_icon_name(i_appName)
@@ -19,25 +17,37 @@ class AppLaunchTest(LauncherBaseTest):
         appOpened = ProcessManager.check_app_by_pid(appPid)
         slash.should.be(appOpened, True)
 
-    def test_launcher(self):
-        for i in range(slash.g.stress_run):
-            slash.logger.info("cycle #%s" % (i + 1))
-            folder_list = ['Files', 'Pictures', 'Videos', 'Music']
-            for folder in folder_list:
-                self.open_from_applauncher_by_icon_name(folder)
-                time.sleep(3)
-            self.assert_and_close_folders()
+    def test_a_launcher(self):
+        log.info('Verify Top Side of App Launcher')
+        folder_list = ['Files', 'Pictures', 'Videos', 'Music']
+        for folder in folder_list:
+            self.open_from_applauncher_by_icon_name(folder)
             time.sleep(3)
-            self.open_app_from_launcher('Galculator')
-	    time.sleep(3)
-            self.contextual_on_calc()
-	    time.sleep(3)
-            #openedFromDesktop = self.open_directory_from_desktop()
-            #time.sleep(3)
-            #slash.should.be(openedFromDesktop, True)
-            #time.sleep(3)
-	    searchedChromium = self.search_chromium()
-            time.sleep(3)
-            slash.should.be(searchedChromium, True)
-   	    slash.g.cycle +=1
-	self.compare_cycle(slash.g.cycle, slash.g.stress_run)
+        launcherWorks = self.assert_and_close_folders()
+        slash.should.be(launcherWorks, True)
+
+    def test_b_app_from_launcher(self):
+        log.info('Contextual tests on app launched by app launcher')
+        self.open_app_from_launcher('Galculator')
+	time.sleep(3)
+        contextualPassed = self.contextual_asserts_on_calc()
+        slash.should.be(contextualPassed, True)
+
+    def test_c_from_desktop(self):
+        log.info('Contextual tests on app launched from desktop')
+        openedFromDesktop = self.open_directory_from_desktop()
+        time.sleep(3)
+        slash.should.be(openedFromDesktop, True)
+
+    def test_d_search(self):
+        log.info('Search Chromium and launch')
+	searchedChromium = self.search_chromium()
+        time.sleep(3)
+        slash.should.be(searchedChromium, True)
+
+    def test_e_bad_search(self):
+        log.info('Search bad text')
+        searchedBadText = self.bad_search()
+        time.sleep(3)
+        slash.should.be(searchedBadText, True)
+
