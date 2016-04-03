87a60cdce578fb38c1fae005455a379cb915b8d6
labuser <sunriver1993@gmail.com>
2016-04-03 14:43:17 +0300

    finalized tests

--- sr_tests/suites/app_launcher/Base.py
+++ sr_tests/suites/app_launcher/Base.py
@@ -2,6 +2,7 @@ from sr_tests.base.Base import BaseTest
 from sr_automation.platform.sunriver.Sunriver import Sunriver
 from sr_automation.platform.linux.applications.Settings.Settings import Settings
 from sr_automation.utils.ImageTools import ImageTools
+from sr_automation.utils.ProcessManager import ProcessManager
 import time
 import getpass
 from bunch import Bunch
@@ -25,37 +26,69 @@ class LauncherBaseTest(BaseTest):
     def open_search(self, i_value=None):
 	log.info('Opening Search')
         slash.g.sunriver.linux.ui.dogtail.rawinput.keyCombo('<Control><Alt>f')
-	if i_value != None:
+	time.sleep(1)
+        if i_value != None:
 		slash.g.sunriver.linux.ui.dogtail.rawinput.typeText(i_value)
    
     def open_directory_from_desktop(self):
-        log.info('Creating folder on desktop')
-        automation_folder = '/home/BigScreen/Desktop/Folder'
-        slash.g.sunriver.linux.ui.dogtail.procedural.os.mkdir(automation_folder)
+        log.info('Creating empty file on desktop')
+        automation_file = '/home/BigScreen/Desktop/File.txt'
+        slash.g.sunriver.linux.ui.dogtail.procedural.os.mknod(automation_file)
         log.info('Testing Side panel from Desktop')
         snapshot = "Desktop_snapshot.png"
-        desktop = "/home/"+self.m_username+"/sr_automation/automation-screenshots/FolderIcon.png"
-        sidebar = "/home/"+self.m_username+"/sr_automation/automation-screenshots/SingleFolder.png"
+        desktop = "/home/"+self.m_username+"/sr_automation/automation-screenshots/Desktop.png"
+        sidebar = "/home/"+self.m_username+"/sr_automation/automation-screenshots/Leafpad-sidebar.png"
+        close_button = "/home/"+self.m_username+"/sr_automation/automation-screenshots/close.png"
+        time.sleep(2)
         folder = ImageTools.find_sub_image_in_image(snapshot, desktop)
-        slash.g.sunriver.linux.ui.dogtail.rawinput.doubleClick(folder.max_location[1]+15, folder.max_location[0]+15)
+        if folder.max_value > 0.9:
+            slash.g.sunriver.linux.ui.dogtail.rawinput.doubleClick(folder.max_location[1]+15, folder.max_location[0]+15)
+        else:
+            log.error('Unable to see file on desktop')
+            return False
+        time.sleep(1)
         sidebar_snap = ImageTools.find_sub_image_in_image(snapshot, sidebar)
         returnValue = False
         if sidebar_snap.max_value > 0.9:
             returnValue = True
-        slash.g.sunriver.linux.ui.dogtail.rawinput.click(sidebar_snap[1]+15, sidebar_snap[0]+15, buttom=3)
+        slash.g.sunriver.linux.ui.dogtail.rawinput.click(sidebar_snap.max_location[1]+15, sidebar_snap.max_location[0]+15, button=3)
         slash.g.sunriver.linux.ui.dogtail.rawinput.absoluteMotion(0,0)
         time.sleep(3)
-        location = ImageTools.find_sub_image_in_image(snapshot, closeall)
+        location = ImageTools.find_sub_image_in_image(snapshot, close_button)
         slash.g.sunriver.linux.ui.dogtail.rawinput.click(location.max_location[1]+15,location.max_location[0]+15)
-        slash.g.sunriver.linux.ui.dogtail.procedural.os.mkdir(automation_folder)
+        slash.g.sunriver.linux.ui.dogtail.procedural.os.remove(automation_file)
         return returnValue
 
     def search_chromium(self):
         log.info('Searching Chromium application')
         chromium = "/home/"+self.m_username+"/sr_automation/automation-screenshots/ChromiumSearch.png"
+        close_button = "/home/"+self.m_username+"/sr_automation/automation-screenshots/close.png"
         snapshot = "Search_snapshot.png"
         self.open_search('Chro')
         imageStats = ImageTools.find_sub_image_in_image(snapshot, chromium)
+        time.sleep(1)
+        slash.g.sunriver.linux.ui.dogtail.rawinput.keyCombo('<Down>')
+        slash.g.sunriver.linux.ui.dogtail.rawinput.keyCombo('<Enter>')
+        sidebar = "/home/"+self.m_username+"/sr_automation/automation-screenshots/Chromium-sidebar.png"
+        time.sleep(3)
+        sidebar_snap = ImageTools.find_sub_image_in_image(snapshot, sidebar)
+        slash.g.sunriver.linux.ui.dogtail.rawinput.click(sidebar_snap.max_location[1]+15, sidebar_snap.max_location[0]+15, button=3)
+        slash.g.sunriver.linux.ui.dogtail.rawinput.absoluteMotion(0,0)
+        time.sleep(1)
+        location = ImageTools.find_sub_image_in_image(snapshot, close_button)
+        slash.g.sunriver.linux.ui.dogtail.rawinput.click(location.max_location[1]+15,location.max_location[0]+15)
+        found = False
+        if imageStats.max_value > 0.9:
+            found = True
+        return found
+
+    def bad_search(self):
+        log.info('Searching non-existent text')
+        bad_text = "/home/"+self.m_username+"/sr_automation/automation-screenshots/BadSearch.png"
+        snapshot = "BadSearch_snap.png"
+        self.open_search('NoTextExist')
+        time.sleep(1)
+        imageStats = ImageTools.find_sub_image_in_image(snapshot, bad_text)
         self.open_search()
         found = False
         if imageStats.max_value > 0.9:
@@ -83,6 +116,7 @@ class LauncherBaseTest(BaseTest):
         if folders_pos.max_value > 0.9:
             foundFolders = True
         slash.should.be(foundFolders, False)
+        return True
 
     def sidebar_manipulation(self, i_x, i_y, i_button=1):
         snapshot = "sidebar_snapshot.png"
@@ -95,7 +129,7 @@ class LauncherBaseTest(BaseTest):
             found = True
         return found
 
-    def contextual_on_calc(self):
+    def contextual_asserts_on_calc(self):
 	log.info('Testing contexual menu on Galculator')
         sidebar_icon = "/home/"+self.m_username+"/sr_automation/automation-screenshots/Galculator-sidebar.png"
         snapshot = "Contextual_snapshot.png"
@@ -125,6 +159,7 @@ class LauncherBaseTest(BaseTest):
         closeLocation = ImageTools.find_sub_image_in_image(snapshot, close_button)
         found = self.sidebar_manipulation(closeLocation.max_location[1]+15,closeLocation.max_location[0]+15)
         slash.should.be(found, False)
+        return True
 
     def open_from_applauncher_by_icon_name(self, i_folderName):
         log.info('Opening '+i_folderName+' from App Launcher')
@@ -136,6 +171,3 @@ class LauncherBaseTest(BaseTest):
         x = location.max_location[1] + 15
         y = location.max_location[0] + 15
         slash.g.sunriver.linux.ui.dogtail.rawinput.click(x,y)
-    
-    def compare_cycle(self,cycle_number, cycle):
-        slash.should.be(cycle_number, cycle)
