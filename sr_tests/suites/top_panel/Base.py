efb9df4a846d5434a2e7d770891dddc40b660783
labuser <sunriver1993@gmail.com>
2016-04-04 14:48:24 +0300

    finalized top panel tests with asserts

--- sr_tests/suites/top_panel/Base.py
+++ sr_tests/suites/top_panel/Base.py
@@ -1,25 +1,71 @@
 from sr_tests.base.Base import BaseTest
 from sr_automation.platform.sunriver.Sunriver import Sunriver
+from sr_automation.utils.ImageTools import ImageTools
 import time
+import getpass
 from bunch import Bunch
 import slash
 
 from logbook import Logger
-log = Logger("Search and App Launcher")
+log = Logger("Top Panel Suite")
 
 class PanelBaseTest(BaseTest):
-   
+
+    m_username = getpass.getuser()
+
     def before(self):
         super(PanelBaseTest, self).before()
     
-    #list containing top panel indicators location in order - userm,applaun,sear,vol,lan,bat,BT,Wifi,sim1,sim2,time,notif,PA
-    slash.g.top_panel_list = [(35,25),(110,25),(140,25),(840,25),(865,25),(900,25),(970,25),(1005,25),(1040,25),(1075,25),(1120,25),(1175,25),(1230,25)]
-	
-    def test_panel(self):
-    	log.info("Opening Top Panel Indicators")
-    	for i in slash.g.top_panel_list: 
-    		slash.g.sunriver.linux.ui.dogtail.rawinput.click(i[0],i[1])
-		time.sleep(3)
-		slash.g.sunriver.linux.ui.dogtail.rawinput.click(i[0],i[1])
-		time.sleep(3)
-		slash.g.sunriver.linux.ui.dogtail.rawinput.click(i[0],i[1])
+    def find_icon_in_top_panel(self, i_IconName):
+        IconPath = "/home/"+self.m_username+"/sr_automation/automation-screenshots/TopPanel"+i_IconName+".png"
+        Snapshot = "IconSnapshot.png"
+        found = False
+        ImageStats = ImageTools.find_sub_image_in_image(Snapshot, IconPath)
+        if ImageStats.max_value > 0.9:
+            found = (ImageStats.max_location[0], ImageStats.max_location[1])
+        return found
+
+    def template_verification(self, i_IconName, i_MenuName):
+        log.info("Verifying "+i_MenuName+" Icon")
+        FoundIcon = self.find_icon_in_top_panel(i_IconName)
+        if FoundIcon is not False:
+            slash.g.sunriver.linux.ui.dogtail.rawinput.click(FoundIcon[1]+15,FoundIcon[0]+15)
+            slash.g.sunriver.linux.ui.dogtail.rawinput.absoluteMotion(0,0)
+            time.sleep(1)
+            OpenedMenu = self.find_icon_in_top_panel(i_IconName+"Menu")
+            slash.g.sunriver.linux.ui.dogtail.rawinput.click(0,0)
+            slash.should.not_be(OpenedMenu, False)
+        else:
+            log.error(i_MenuName+" Icon missing")
+            slash.should.not_be(FoundIcon, False)
+
+    def test_battery(self):
+        log.info("Verifying Battery Icon")
+        FoundIcon = self.find_icon_in_top_panel("Battery")
+        slash.should.not_be(FoundIcon, False)
+
+    def test_switch_to_phone(self):
+        log.info("Verifying Switch to Phone Icon")
+        FoundIcon = self.find_icon_in_top_panel("SwitchToPhone")
+        slash.should.not_be(FoundIcon, False)
+
+    def test_search(self):
+        self.template_verification("Search", "Search")
+
+    def test_app_launcher(self):
+        self.template_verification("Applications", "App Launcher")
+
+    def test_user_menu(self):
+        self.template_verification("UserMenu", "User Menu")
+
+    def test_language(self):
+        self.template_verification("Language", "Language")
+
+    def test_notifications(self):
+        self.template_verification("Notifications", "Notifications")
+
+    def test_phone_app(self):
+        self.template_verification("Phone_App", "Phone App")
+    
+    def test_bluetooth(self):
+        self.template_verification("Bluetooth", "Bluetooth")
