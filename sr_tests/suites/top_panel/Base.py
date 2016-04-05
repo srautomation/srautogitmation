190ac4cf515930bc37698b0fdbb4432b2150dccd
labuser <sunriver1993@gmail.com>
2016-04-05 15:13:24 +0300

    finalized all tests

--- sr_tests/suites/top_panel/Base.py
+++ sr_tests/suites/top_panel/Base.py
@@ -5,6 +5,7 @@ import time
 import getpass
 from bunch import Bunch
 import slash
+import datetime
 
 from logbook import Logger
 log = Logger("Top Panel Suite")
@@ -38,6 +39,29 @@ class PanelBaseTest(BaseTest):
         else:
             log.error(i_MenuName+" Icon missing")
             slash.should.not_be(FoundIcon, False)
+    
+    def test_clock(self):
+        log.info("Verifying Clock Icon")
+        NotifPath = "/home/"+self.m_username+"/sr_automation/automation-screenshots/TopPanelNotifications.png"
+        Snapshot = "ClockSnapshot.png"
+        current_time = datetime.datetime.now().time()
+        NotifStats = ImageTools.find_sub_image_in_image(Snapshot, NotifPath)
+        x_min = int(NotifStats.max_location[1]-75)
+        x_max = int(NotifStats.max_location[1])
+        ClockCrop = {
+                    "y_min" : 0,
+                    "y_max" : 50,
+                    "x_min" : x_min,
+                    "x_max" : x_max,
+                    "z_min": 0,
+                    "z_max" : 3
+                    }
+        displayed_time = ImageTools.ocr_image(Snapshot, ClockCrop)
+        CorrectClock = True
+        for i in range(2):
+            if str(current_time)[i] != displayed_time[i]:
+                CorrectClock = False
+        slash.should.be(CorrectClock, True)
 
     def test_battery(self):
         log.info("Verifying Battery Icon")
@@ -47,6 +71,11 @@ class PanelBaseTest(BaseTest):
     def test_switch_to_phone(self):
         log.info("Verifying Switch to Phone Icon")
         FoundIcon = self.find_icon_in_top_panel("SwitchToPhone")
+        slash.should.not_be(FoundIcon, False)
+
+    def test_network_wifi(self):
+        log.info("Verifying Network & Wi-Fi Icons")
+        FoundIcon = self.find_icon_in_top_panel("NetworkWiFi")
         slash.should.not_be(FoundIcon, False)
 
     def test_search(self):
