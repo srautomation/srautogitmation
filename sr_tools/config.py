d1c32780265a5dff8ac390f24e16bcf2d90862b8
labuser <sunriver1993@gmail.com>
2016-04-07 14:30:05 +0300

    added config for sanity run

--- sr_tools/config.py
+++ sr_tools/config.py
@@ -2,3 +2,10 @@
 automation_files_dir = "/tmp/automation-temp/"
 pictures_dir = automation_files_dir + "automation-screenshots/"
 chroot_path = "/data/sunriver/fs/limited/"
+log_dir = "/tmp/automation-logs/"
+sanity_suites = [
+                ('top_panel','Base.py'),
+                ('phone_app','Base.py'),
+                ('app_launcher','AppLauncher.py'),
+                ('seemless_browser','seemless_sanity.py')
+                ]
