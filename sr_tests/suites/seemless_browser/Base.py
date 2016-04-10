5c03d0bb11b747c9018f3e97707958ec58df8352
labuser <sunriver1993@gmail.com>
2016-04-10 17:29:46 +0300

    half automatic

--- sr_tests/suites/seemless_browser/Base.py
+++ sr_tests/suites/seemless_browser/Base.py
@@ -19,19 +19,3 @@ class SeemlessBrowserTest(BaseTest):
         log.info("Starting LibreOffice")
         slash.g.writer = Writer(slash.g.sunriver.linux)
         slash.g.writer.start(option='/tmp/dogtail-BigScreen/chromeShot.png')
-
-    def compare_cycle(self,cycle_number, cycle):
-        slash.should.be(cycle_number, cycle)
-
-    def compare_url(self, urlA, urlB):
-        if urlA in urlB:
-            log.info('correct')
-        else:
-            log.info('not the expected url')
-        
-    def compare_chrome_android(self, url):
-        if slash.g.sunriver.android.ui(text=url).exists:
-            assert 1 == 1 #needs to be changed to something realistic
-        else:
-            slash.add_error('The URLS are not the same')
-        
