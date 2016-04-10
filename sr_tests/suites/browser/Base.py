f441c3e45cd86ac54a5381f482a2d22ab99a126e
labuser <sunriver1993@gmail.com>
2016-04-10 10:12:03 +0300

    obsolete test

--- sr_tests/suites/browser/Base.py
+++ sr_tests/suites/browser/Base.py
@@ -1,8 +1,1 @@
-from sr_tests.base.Base import BaseTest
-from sr_automation.platform.linux.applications.Browser.Browser import Chromium
-
-class BrowserBaseTest(BaseTest):
-    def test_chromium(self):
-        self.browser = Browser.Browser(self.linux)
-        self.browser.start()
-        return self.browser.chromium
+#this folder is obsolete
