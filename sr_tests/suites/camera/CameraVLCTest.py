fbb1195e2c70ba5493731ccf4c81759e35d44aa1
labuser <sunriver1993@gmail.com>
2016-04-10 12:09:28 +0300

    cleanup for video test

--- sr_tests/suites/camera/CameraVLCTest.py
+++ sr_tests/suites/camera/CameraVLCTest.py
@@ -1,22 +1,23 @@
 import slash
+from sr_automation.platform.sunriver.Sunriver import Sunriver
 from Base import CameraBaseTest
 import time
 from logbook import Logger
-log = Logger("StressBaseInsideTest")
+log = Logger("Camera and VLC")
 
 class CameraVLCTest(CameraBaseTest):
-
-    @slash.hooks.session_start.register
-    def start_video():
-        log.info('starting camera test')
+    
+    m_VideoDuration = 60
+    m_VideoPath = "/home/BigScreen/Android/DCIM/Camera/"
 
     def test_camera(self):
-        if not self.check_vnc_open():
-            slash.g.sunriver.linux.ui.dogtail.rawinput.keyCombo('<Control>h')
+        if not slash.g.sunriver.vnc.isVNCOpen():
+            slash.g.sunriver.vnc.OpenVnc()
         self.open_camera()
-        self.start_stop_recording()
-        time.sleep(15)
-        self.start_stop_recording()
-	self.open_vlc()
-	 
-
+        self.record_by_duration(self.m_VideoDuration)
+        video_dir = slash.g.sunriver.linux.ui.dogtail.procedural.os.listdir(self.m_VideoPath)
+        slash.should.be(len(video_dir), 1)
+        video_name = video_dir[0]
+        self.play_video_in_device(video_name, self.m_VideoDuration)
+        self.play_video_in_vlc(video_name, self.m_VideoDuration)
+        slash.g.sunriver.linux.ui.dogtail.procedural.os.remove(self.m_VideoPath+video_name)
