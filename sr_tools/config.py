afb9d8a1023cb60c8a40d5afcf897409551e33fc
labuser <sunriver1993@gmail.com>
2016-04-12 17:13:43 +0300

    added mail to sanity group

--- sr_tools/config.py
+++ sr_tools/config.py
@@ -7,7 +7,7 @@ sanity_suites = [
                 ('top_panel','Base.py'),
                 ('phone_app','Base.py'),
                 ('app_launcher','AppLauncher.py'),
-                #mail
+                ('mail','SanityMailTest.py'),
                 #('pcmanfm','Base.py'),
                 ('seamless_browser','seamless_sanity.py'),
                 ('camera','CameraVLCTest.py')
