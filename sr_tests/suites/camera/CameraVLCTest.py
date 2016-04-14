283820bc4cd712d896af65d9b9cebc6e94a4527c
labuser <sunriver1993@gmail.com>
2016-04-14 15:39:48 +0300

    logging

--- sr_tests/suites/camera/CameraVLCTest.py
+++ sr_tests/suites/camera/CameraVLCTest.py
@@ -1,5 +1,4 @@
 import slash
-from sr_automation.platform.sunriver.Sunriver import Sunriver
 from Base import CameraBaseTest
 import time
 from logbook import Logger
@@ -11,13 +10,20 @@ class CameraVLCTest(CameraBaseTest):
     m_VideoPath = "/home/BigScreen/Android/DCIM/Camera/"
 
     def test_camera(self):
-        slash.g.sunriver.vnc.OpenVnc()
+        self.sunriver.vnc.OpenVnc()
+        time.sleep(2)
         self.open_camera()
+        time.sleep(2)
         self.record_by_duration(self.m_VideoDuration)
-        video_dir = slash.g.sunriver.linux.ui.dogtail.procedural.os.listdir(self.m_VideoPath)
+        video_dir = self.dogtail.procedural.os.listdir(self.m_VideoPath)
         slash.should.be(len(video_dir), 1)
         video_name = video_dir[0]
         self.play_video_in_device(video_name, self.m_VideoDuration)
-        self.play_video_in_vlc(video_name, self.m_VideoDuration)
-        slash.g.sunriver.linux.ui.dogtail.procedural.os.remove(self.m_VideoPath+video_name)
-        slash.g.sunriver.vnc.CloseVnc()
+        try:
+            self.play_video_in_vlc(video_name, self.m_VideoDuration)
+        except TypeError:
+            slash.add_error("Error in reading recorded file via VLC", 0)
+        for vid_name in self.dogtail.procedural.os.listdir(self.m_VideoPath):
+            path = self.dogtail.procedural.os.path.join(self.m_VideoPath, vid_name)
+            self.dogtail.procedural.os.remove(path)
+        self.sunriver.vnc.CloseVnc()
