c2b04718c660e85947320cc32774e166861a21ce
labuser <sunriver1993@gmail.com>
2016-04-20 14:25:53 +0300

    added handling issue when camera doesn't start up

--- sr_tests/suites/camera/CameraVLCTest.py
+++ sr_tests/suites/camera/CameraVLCTest.py
@@ -9,10 +9,20 @@ class CameraVLCTest(CameraBaseTest):
     m_VideoDuration = 60
     m_VideoPath = "/home/BigScreen/Android/DCIM/Camera/"
 
+    def clean_recorded_folder(self):
+        log.info("Clearing past recordings")
+        for vid_name in self.dogtail.procedural.os.listdir(self.m_VideoPath):
+            path = self.dogtail.procedural.os.path.join(self.m_VideoPath, vid_name)
+            self.dogtail.procedural.os.remove(path)
+            
     def test_camera(self):
         self.sunriver.vnc.OpenVnc()
-        time.sleep(2)
+        self.clean_recorded_folder()
+        time.sleep(1)
         self.open_camera()
+        if not self.androidUI(resourceId="com.android.camera2:id/shutter_button").exists:
+            slash.add_error("Unable to open camera")
+            slash.skip_test()
         time.sleep(2)
         self.record_by_duration(self.m_VideoDuration)
         video_dir = self.dogtail.procedural.os.listdir(self.m_VideoPath)
@@ -23,7 +33,4 @@ class CameraVLCTest(CameraBaseTest):
             self.play_video_in_vlc(video_name, self.m_VideoDuration)
         except TypeError:
             slash.add_error("Error in reading recorded file via VLC", 0)
-        for vid_name in self.dogtail.procedural.os.listdir(self.m_VideoPath):
-            path = self.dogtail.procedural.os.path.join(self.m_VideoPath, vid_name)
-            self.dogtail.procedural.os.remove(path)
         self.sunriver.vnc.CloseVnc()
