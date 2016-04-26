ed6e94ffea01c63c25bcdb1796b286090ebb3639
labuser <sunriver1993@gmail.com>
2016-04-26 12:24:10 +0300

    new search tests

--- sr_tests/suites/app_launcher/Base.py
+++ sr_tests/suites/app_launcher/Base.py
@@ -5,6 +5,7 @@ import getpass
 import slash
 
 from logbook import Logger
+from __builtin__ import False
 log = Logger("Search, App Launcher and Side Panel")
 
 class LauncherBaseTest(BaseTest):
@@ -24,70 +25,13 @@ class LauncherBaseTest(BaseTest):
         time.sleep(1)
         if i_value != None:
             slash.g.sunriver.linux.ui.dogtail.rawinput.typeText(i_value)
-   
-    def open_directory_from_desktop(self):
-        log.info('Creating empty file on desktop')
-        automation_file = '/home/BigScreen/Desktop/File.txt'
-        slash.g.sunriver.linux.ui.dogtail.procedural.os.mknod(automation_file)
-        log.info('Testing Side panel from Desktop')
-        snapshot = "Desktop_snapshot.png"
-        desktop = "/home/"+self.m_username+"/sr_automation/automation-screenshots/Desktop.png"
-        sidebar = "/home/"+self.m_username+"/sr_automation/automation-screenshots/Leafpad-sidebar.png"
-        close_button = "/home/"+self.m_username+"/sr_automation/automation-screenshots/close.png"
-        time.sleep(2)
-        folder = ImageTools.find_sub_image_in_image(snapshot, desktop)
-        if folder.max_value > 0.9:
-            slash.g.sunriver.linux.ui.dogtail.rawinput.doubleClick(folder.max_location[1]+15, folder.max_location[0]+15)
-        else:
-            log.error('Unable to see file on desktop')
-            return False
-        time.sleep(1)
-        sidebar_snap = ImageTools.find_sub_image_in_image(snapshot, sidebar)
-        returnValue = False
-        if sidebar_snap.max_value > 0.9:
-            returnValue = True
-        slash.g.sunriver.linux.ui.dogtail.rawinput.click(sidebar_snap.max_location[1]+15, sidebar_snap.max_location[0]+15, button=3)
-        slash.g.sunriver.linux.ui.dogtail.rawinput.absoluteMotion(0,0)
-        time.sleep(3)
-        location = ImageTools.find_sub_image_in_image(snapshot, close_button)
-        slash.g.sunriver.linux.ui.dogtail.rawinput.click(location.max_location[1]+15,location.max_location[0]+15)
-        slash.g.sunriver.linux.ui.dogtail.procedural.os.remove(automation_file)
-        return returnValue
-
-    def search_chromium(self):
-        log.info('Searching Chromium application')
-        chromium = "/home/"+self.m_username+"/sr_automation/automation-screenshots/ChromiumSearch.png"
-        close_button = "/home/"+self.m_username+"/sr_automation/automation-screenshots/close.png"
-        snapshot = "Search_snapshot.png"
-        self.open_search('Chro')
-        time.sleep(2)
-        imageStats = ImageTools.find_sub_image_in_image(snapshot, chromium)
-        slash.g.sunriver.linux.ui.dogtail.rawinput.keyCombo('<Down>')
-        slash.g.sunriver.linux.ui.dogtail.rawinput.keyCombo('<Enter>')
-        sidebar = "/home/"+self.m_username+"/sr_automation/automation-screenshots/Chromium-sidebar.png"
-        time.sleep(3)
-        sidebar_snap = ImageTools.find_sub_image_in_image(snapshot, sidebar)
-        slash.g.sunriver.linux.ui.dogtail.rawinput.click(sidebar_snap.max_location[1]+15, sidebar_snap.max_location[0]+15, button=3)
-        slash.g.sunriver.linux.ui.dogtail.rawinput.absoluteMotion(0,0)
-        time.sleep(1)
-        location = ImageTools.find_sub_image_in_image(snapshot, close_button)
-        slash.g.sunriver.linux.ui.dogtail.rawinput.click(location.max_location[1]+15,location.max_location[0]+15)
+    
+    def give_image_location_if_found(self, i_ImageToFind):
+        snapshot = "Snapshot.png"
+        foundImage = ImageTools.find_sub_image_in_image(snapshot, i_ImageToFind)
         found = False
-        if imageStats.max_value > 0.9:
-            found = True
-        return found
-
-    def bad_search(self):
-        log.info('Searching non-existent text')
-        bad_text = "/home/"+self.m_username+"/sr_automation/automation-screenshots/BadSearch.png"
-        snapshot = "BadSearch_snap.png"
-        self.open_search('NoTextExist')
-        time.sleep(2)
-        imageStats = ImageTools.find_sub_image_in_image(snapshot, bad_text)
-        self.open_search()
-        found = False
-        if imageStats.max_value > 0.9:
-            found = True
+        if foundImage.max_value > 0.9:
+            found = foundImage.max_location
         return found
 
     def assert_and_close_folders(self):
