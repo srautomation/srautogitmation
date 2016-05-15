9c844b8392076e0ab7ddcf84823431bd4aee42e6
labuser <sunriver1993@gmail.com>
2016-05-15 12:46:43 +0300

    fixed delay for recorded video

--- sr_tests/suites/camera/CameraVLCTest.py
+++ sr_tests/suites/camera/CameraVLCTest.py
@@ -2,6 +2,7 @@ import slash
 from Base import CameraBaseTest
 import time
 from logbook import Logger
+from termcolor import colored
 log = Logger("Camera and VLC")
 
 class CameraVLCTest(CameraBaseTest):
@@ -16,8 +17,10 @@ class CameraVLCTest(CameraBaseTest):
             self.dogtail.procedural.os.remove(path)
             
     def test_camera(self):
+        log.info('Testing recording and playing video')
         self.sunriver.vnc.OpenVnc()
         self.clean_recorded_folder()
+        raw_input(colored('Press Enter to start recording:','green'))
         self.open_camera()
         time.sleep(4)
         if not self.androidUI(resourceId="com.android.camera2:id/shutter_button").exists:
@@ -25,10 +28,10 @@ class CameraVLCTest(CameraBaseTest):
             slash.skip_test()
         time.sleep(2)
         self.record_by_duration(self.m_VideoDuration)
+        time.sleep(5)
         video_dir = self.dogtail.procedural.os.listdir(self.m_VideoPath)
         assert len(video_dir) == 1 , "too many recorded videos"
         video_name = video_dir[0]
-        time.sleep(5)
         self.play_video_in_device(video_name, self.m_VideoDuration)
         assert slash.g.sunriver.android.ui(text="Can't play this video.").exists == False , "Can not play video in device"
         try:
@@ -40,6 +43,6 @@ class CameraVLCTest(CameraBaseTest):
     def test_big_buck_bunny(self):
         log.info("Testing Big Buck Bunny")
         try:
-            slash.g.sunriver.linux.ui.dogtail.procedural.os.system('vlc --play-and-exit --fullscreen "/home/BigScreen/SDCard/Movies/h264_big_buck_bunny_480p.mov"')
+            slash.g.sunriver.linux.ui.dogtail.procedural.os.system('vlc --play-and-exit --fullscreen "/home/BigScreen/SDCard/mpeg4_big_buck_bunny_720p_surround.avi"')
         except:
             slash.add_error("Unable to play Big Buck Bunny in VLC")
